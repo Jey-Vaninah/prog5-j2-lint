834732e5ab6d53d742a6a3ceb0ed7f50a8350012
Jey-Vaninah <hei.vaninah@gmail.com>
2025-11-21 14:04:04 +0300

    feat: wallet class

--- src/wallet.ts
+++ src/wallet.ts
@@ -1,2 +1,54 @@
-const message: string = "Hello TypeScript!";
-console.log(message);
+class Wallet {
+    private color: string;
+    private size: number;
+    private money: number;
+    private opened: boolean;
+    private lost: boolean;
+
+    constructor(color: string, size: number) {
+        this.color = color;
+        this.size = size;
+        this.money = 0;
+        this.opened = false;
+        this.lost = false;
+    }
+
+    private notify(message: string): void {
+        void message;
+    }
+
+    public getMoney(): number {
+        return this.money
+    }
+
+    public addMoney(amount: number): void {
+        if (this.lost) {
+            this.notify("Cannot add money the wallet is lost");
+            return;
+        }
+        this.money += amount;
+    }
+
+    public open(): void {
+        if (this.lost) {
+            this.notify("Cannot open: the wallet is lost");
+            return;
+        }
+        else {
+            this.opened = true;
+        }
+    }
+
+    public close(): void {
+        this.opened = false
+    }
+
+    public isLost(): boolean {
+        return this.lost;
+    }
+
+    public checkMoney(): string {
+        return (`Available money: ${this.money} €`);
+    }
+}
+
